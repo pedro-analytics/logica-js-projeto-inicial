aee17e8f998f0c134534fc0cc6956bf30515d653
pedro-analytics <ppanalytics@outlook.com>
2025-07-30 16:41:29 -0300

    verificando o chute

--- Explore funções e listas/js-curso-2-aula1/app.js
+++ Explore funções e listas/js-curso-2-aula1/app.js
@@ -4,7 +4,7 @@
 // let paragrafo = document.querySelector("p");
 // paragrafo.innerHTML = "Escolha um número entre 1 e 10";
 
-let numeroSecreto = generateRandomNumber();
+let numeroSecreto = 7;
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
@@ -16,7 +16,16 @@ exibirTextoNaTela("p", "Escolha um número entre 1 e 10");
 
 function verificarChute() {
     let chute = document.querySelector("input").value;
-    console.log(chute == numeroSecreto);
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela("h1", "Acertou!");
+        exibirTextoNaTela("p", "Você acertou o número secreto!");
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela("p", "O número secreto é menor.");
+        } else{
+            exibirTextoNaTela("p", "O número secreto é maior.");
+        }
+    }
 }
 
 function generateRandomNumber() {
